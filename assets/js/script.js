613d6dd59b9ef3ff7dc88c18839582a305b0a74c
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-07-22 18:25:29 +0100

    work with API - show latest movie posters

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,18 @@ const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMGPath = "https://image.tmdb.org/t/p/w1280";
 const SearchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=" + APIKey +"&query=";
 
+async function movies() {
+  const resp = await fetch(APIURL);
+  const respData = await resp.json();
+
+  respData.results.forEach((movie) => {
+    const img = document.createElement("img");
+    img.src = IMGPath + movie.poster_path
+
+    document.body.appendChild(img);
+  });
+
+  return respData;
+}
+
+movies();
