c3e737e18cf5a2dff1b140f64bc946ea8c02f781
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-07-22 18:12:48 +0100

    add API and links

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// API link with key, please follow readme how to create own API key for application to work.
+
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + APIKey + "&page-1";
+const IMGPath = "https://image.tmdb.org/t/p/w1280";
+const SearchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=" + APIKey +"&query=";
+
